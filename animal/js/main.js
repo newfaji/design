3661c381af14f848c3c60a5e1b343a2108b96849
newfaji <158270331+newfaji@users.noreply.github.com>
2024-02-23 17:58:29 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -25,20 +25,78 @@ $(document).ready(function(){
 
     /* swiper */
     const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-
-        // autoplay: {  /* 팝업 자동 실행 */
-        //     delay: 3000,
-        //     disableOnInteraction: true,
-        // },
-
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 1000,
+            disableOnInteraction: true,
+        },
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
         navigation: {  /* 이전, 다음 버튼 */
             nextEl: '.visual .next',  /* 다음 버튼의 클래스명 */
             prevEl: '.visual .prev',  
         },
-
     });
-    swiper.autoplay.stop();  /* 일시정지 기능 */
-    swiper.autoplay.start();  /* 재생 기능 */
+
+    /* 
+        .visual .ctrl_btn .stop 정지 버튼을 클릭하면 
+        팝업은 정지됨, stop사라짐, play버튼나타남
+        .visual .ctrl_btn .play 재생버튼을 클릭하면
+        팝업은 재생되고, stop버튼은 나타나고, play사라짐
+    */
+    $('.visual .ctrl_btn .stop').on('click', function(){
+        swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide()
+        $('.visual .ctrl_btn .play').show()
+    })
+    $('.visual .ctrl_btn .play').on('click', function(){
+        swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide()
+        $('.visual .ctrl_btn .stop').show()
+    })
+
+    /*
+        .lost .list > ul > li 클릭하면
+        li에 active 클래스 추가
+        이전에 active클래스가 있던 li의 active는 삭제
+    */
+    $('.lost .list > ul > li').on('click', function(){
+        $('.lost .list > ul > li').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    /* 입양 팝업 */
+    const adopt_swiper = new Swiper('.adopt .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 0, /* 팝업과 팝업 사이 여백 */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        breakpoints: {
+            500: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 0,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 0,
+            },
+            1320: {
+                slidesPerView: 'auto', /* 사이즈제한 */
+                spaceBetween: 0,
+            },
+        },
+        navigation: {
+            nextEl: '.adopt .next',
+            prevEl: '.adopt .prev',
+        },
+    });
+    // adopt_swiper.autoplay.stop();  /* 일시정지 기능 */
+    // adopt_swiper.autoplay.start();  /* 재생 기능 */
+    $('.adopt .ctrl_btn .stop').on('click', function(){
+        adopt_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide()
+        $('.adopt .ctrl_btn .play').show()
+    })
+    $('.adopt .ctrl_btn .play').on('click', function(){
+        adopt_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide()
+        $('.adopt .ctrl_btn .stop').show()
+    })
 })
