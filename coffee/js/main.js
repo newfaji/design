6fe43a4dfeb8664e3c2be06ca0c8a85d59e0a86e
newfaji <158270331+newfaji@users.noreply.github.com>
2024-03-18 14:34:52 +0900

    .

--- coffee/js/main.js
+++ coffee/js/main.js
@@ -30,22 +30,16 @@ $(document).ready(function(){
     });
 
     const social_swiper = new Swiper('.social .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-        breakpoints: {
-            768: {    /* 768px 이상일때 적용 */
-                slidesPerView: 2,
-                spaceBetween: 16,
-            },
-            1024: {   /* 1024px 이상일때 적용 */
-                slidesPerView: 3,
-                spaceBetween: 17,
-            },
-            1280: {    /* 1280px 이상일때 적용 */
-                slidesPerView: 4,
-                spaceBetween: 17,
-            },
-        },
+        centeredSlides: true,
+        loop: true,
+    });
+
+    AOS.init({
+        offset: 300, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+        duration: 500, // 애니메이션 효과가 작동되는 시간
+        easing: 'ease', // 가속도
     });
 
 })//$(document).ready
